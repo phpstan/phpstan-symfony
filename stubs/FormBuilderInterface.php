548bd9cddbfc374ba2fa66ca915307398ba52c4c
Vasek Purchart <me@vasekpurchart.cz>
2019-12-07 06:51:25 +0100

    Add stubs for (some) Symfony iterable types

---
+++ stubs/FormBuilderInterface.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Symfony\Component\Form;
+
+/**
+ * @extends \Traversable<int, \Symfony\Component\Form\FormBuilderInterface>
+ */
+interface FormBuilderInterface extends \Traversable
+{
+
+}
