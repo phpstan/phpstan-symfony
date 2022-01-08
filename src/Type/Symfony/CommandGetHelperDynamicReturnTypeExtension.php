d31922ca4bab5684d53dc749d5742f0b455c0149
Lukáš Unger <looky.msc@gmail.com>
2022-01-08 19:51:27 +0100

    Command::getHelper return type

---
+++ src/Type/Symfony/CommandGetHelperDynamicReturnTypeExtension.php
@@ -0,0 +1,71 @@
+<?php declare(strict_types = 1);
+
+namespace PHPStan\Type\Symfony;
+
+use PhpParser\Node\Expr\MethodCall;
+use PHPStan\Analyser\Scope;
+use PHPStan\Reflection\MethodReflection;
+use PHPStan\Symfony\ConsoleApplicationResolver;
+use PHPStan\Type\DynamicMethodReturnTypeExtension;
+use PHPStan\Type\ObjectType;
+use PHPStan\Type\Type;
+use PHPStan\Type\TypeCombinator;
+use PHPStan\Type\TypeUtils;
+use Throwable;
+use function count;
+use function get_class;
+
+final class CommandGetHelperDynamicReturnTypeExtension implements DynamicMethodReturnTypeExtension
+{
+
+	/** @var \PHPStan\Symfony\ConsoleApplicationResolver */
+	private $consoleApplicationResolver;
+
+	public function __construct(ConsoleApplicationResolver $consoleApplicationResolver)
+	{
+		$this->consoleApplicationResolver = $consoleApplicationResolver;
+	}
+
+	public function getClass(): string
+	{
+		return 'Symfony\Component\Console\Command\Command';
+	}
+
+	public function isMethodSupported(MethodReflection $methodReflection): bool
+	{
+		return $methodReflection->getName() === 'getHelper';
+	}
+
+	public function getTypeFromMethodCall(MethodReflection $methodReflection, MethodCall $methodCall, Scope $scope): Type
+	{
+		$defaultReturnType = new ObjectType('Symfony\Component\Console\Helper\HelperInterface');
+
+		if (!isset($methodCall->getArgs()[0])) {
+			return $defaultReturnType;
+		}
+
+		$classReflection = $scope->getClassReflection();
+		if ($classReflection === null) {
+			return $defaultReturnType;
+		}
+
+		$argStrings = TypeUtils::getConstantStrings($scope->getType($methodCall->getArgs()[0]->value));
+		if (count($argStrings) !== 1) {
+			return $defaultReturnType;
+		}
+		$argName = $argStrings[0]->getValue();
+
+		$returnTypes = [];
+		foreach ($this->consoleApplicationResolver->findCommands($classReflection) as $command) {
+			try {
+				$command->mergeApplicationDefinition();
+				$returnTypes[] = new ObjectType(get_class($command->getHelper($argName)));
+			} catch (Throwable $e) {
+				// no-op
+			}
+		}
+
+		return count($returnTypes) > 0 ? TypeCombinator::union(...$returnTypes) : $defaultReturnType;
+	}
+
+}
