117ef6d18740890b9fba1601a91320edf8f396c3
Gocha Ossinkine <ossinkine@ya.ru>
2022-01-16 23:41:59 +0200

    Post request variables can be only string

--- tests/Type/Symfony/data/input_bag_from_request.php
+++ tests/Type/Symfony/data/input_bag_from_request.php
@@ -10,11 +10,11 @@ class Foo
 
 	public function doFoo(Request $request): void
 	{
-		assertType('bool|float|int|string|null', $request->request->get('foo'));
+		assertType('string|null', $request->request->get('foo'));
 		assertType('string|null', $request->query->get('foo'));
 		assertType('string|null', $request->cookies->get('foo'));
 
-		assertType('bool|float|int|string', $request->request->get('foo', 'foo'));
+		assertType('string', $request->request->get('foo', 'foo'));
 		assertType('string', $request->query->get('foo', 'foo'));
 		assertType('string', $request->cookies->get('foo', 'foo'));
 	}
