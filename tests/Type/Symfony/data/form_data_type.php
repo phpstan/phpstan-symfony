1a263bc0c52d8003527879ec879e75b53f80ce15
Ondřej Mirtes <ondrej@mirtes.cz>
2023-04-14 14:15:12 +0200

    Update form_data_type.php

--- tests/Type/Symfony/data/form_data_type.php
+++ tests/Type/Symfony/data/form_data_type.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types = 1);
 
-namespace PHPStan\Type\Symfony;
+namespace GenericFormDataType;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
