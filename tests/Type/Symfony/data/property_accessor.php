a32bc86da24495025d7aafd1ba62444d4a364a98
Vincent Langlet <VincentLanglet@users.noreply.github.com>
2024-03-16 17:50:20 +0100

    Add `@param-out` for PropertyAccessor
    * Add param out for propertyAccessor
    
    * Fix stub

---
+++ tests/Type/Symfony/data/property_accessor.php
@@ -0,0 +1,13 @@
+<?php declare(strict_types = 1);
+
+use function PHPStan\Testing\assertType;
+
+$propertyAccessor = new \Symfony\Component\PropertyAccess\PropertyAccessor();
+
+$array = [1 => 'ea'];
+$propertyAccessor->setValue($array, 'foo', 'bar');
+assertType('array', $array);
+
+$object = new \stdClass();
+$propertyAccessor->setValue($object, 'foo', 'bar');
+assertType('stdClass', $object);
